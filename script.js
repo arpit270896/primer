6a23855e48998cd5c9e6f10148a47f13d093c29e
Arpit Singla <arpit270896@gmail.com>
2017-03-18 01:54:38 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+var ctx = null;
+var canvas = document.getElementById("tmpImage");
+var localMediaStream = null;
+var video = document.querySelector('video');
+
+function snapshot() {        
+		if (localMediaStream) {
+				ctx.drawImage(video, 0, 0);
+				var img = document.getElementById('CaptureImage');
+				// "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
+				img.src = canvas.toDataURL('image/jpg');
+		}
+}    
+
+function hasGetUserMedia() {
+		// Note: Opera builds are unprefixed.
+		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+				navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+function onFailSoHard(){
+
+}
+
+function start() {
+		if (hasGetUserMedia()) {
+				if (navigator.webkitGetUserMedia)
+						navigator.getUserMedia = navigator.webkitGetUserMedia;
+				//var getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
+
+				
+				//var gumOptions = { video: true, toString: function () { return 'video'; } };    
+				if (navigator.getUserMedia) {
+						navigator.getUserMedia({ video: true, audio: true }, function (stream) {
+								if (navigator.webkitGetUserMedia) {
+										video.src = window.webkitURL.createObjectURL(stream);
+								} else {
+										video.src = stream; // Opera
+								}
+								localMediaStream = stream;
+						}, onFailSoHard);
+				} else {
+						video.src = 'somevideo.webm'; // fallback.
+				}
+		}
+}
+
+function stop() {
+		video = document.getElementById('sourcevid');
+		video.src = "";
+}
+
+function ResizeCanvas() {
+		canvas.height = video.videoHeight;
+		canvas.width = video.videoWidth;
+}
+
+$(document).ready(function () {
+		$('.button').button();
+		ctx = canvas.getContext('2d');
+});
