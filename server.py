7ffe4a45780e8b01db8419ca33c211eb2b38ccc7
Arpit Singla <arpit270896@gmail.com>
2017-03-18 04:12:19 +0530

    Mostly done

--- server.py
+++ server.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, request
 from base64 import b64decode
 import random
 import os
+import string
 
-def randomword(length = 50):
-   return ''.join(random.choice(string.lowercase) for i in range(length))
 
 app = Flask(__name__)
 
@@ -17,15 +16,23 @@ def get_image():
 	# img_base64 = request.data.get('image_data')
 	img_base64 = request.form['image_data'].split('data:image/png;base64,')[1]
 
-	hash = randomword()
+	rand_string = random.getrandbits(128)
 
-	with open("static/photos/{}.jpg".format(hash), 'wb') as f:
+	with open("static/photos/{}.jpg".format(rand_string), 'wb') as f:
 		f.write(b64decode(img_base64))
 
-	os.system("./primitive static/photos/{}.jpg /tmp/primitive/{}.gif && mv /tmp/primitive/{}.gif static/gifs/{}.gif &")
+	os.system("primitive -i static/photos/{0}.jpg -o /tmp/{0}.gif -n 100 -m 6 && mv /tmp/{0}.gif static/gifs/{0}.gif".format(rand_string))
 
-	return "fuck"
+	return "static/gifs/{0}.gif".format(rand_string)
 
+@app.route("/mail", methods=['POST'])
+def mail():
+	mail_id = request.form['email']
+	rand_string = request.form['hash']
+
+	# Mail here!
+
+	return "Success!"
 
 if __name__ == "__main__":
     app.run(debug=True)
