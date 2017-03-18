8e1d6f178fd3044e6458d6276a4a16dc251e206f
Arpit Singla <arpit270896@gmail.com>
2017-03-18 08:28:42 +0530

    Fix mailing system

--- server.py
+++ server.py
@@ -5,7 +5,10 @@ import random
 import string
 import os
 from config import config
-
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 app = Flask(__name__)
 
 
@@ -33,21 +36,37 @@ def get_image():
 def mail():
 	mail_id = request.form['email']
 	rand_string = request.form['hash']
+	
+	msg = MIMEMultipart()
+	msg.attach(MIMEText('Hello from Mailgun'))
+	msg['Subject'] = "Hello from SDSLabs"
+	msg['From']    = "convop@srishti.sdslabs.co"
+	msg['To']      = mail_id
 
-	request_url = 'https://api.mailgun.net/v3/{0}/messages'.format(config['mailgun_sandbox'])
-	r = requests.post(
-		request_url,
-		auth=('api', config['mailgun_key']),
-		data={
-			'from': 'postmaster@srishti.sdslabs.co',
-			'to': mail_id,
-			'subject': 'Hello from SDSLabs',
-			'text': 'Hello from Mailgun'
-		},
-		files=[("attachment", open("static/gifs/{}.gif".format(rand_string), 'rb'))]
-	)
+	part = MIMEApplication(open("static/gifs/{}.gif".format(rand_string), 'rb').read(),Name=rand_string+".gif")
+	part['Content-Disposition'] = "attachment; filename={}.gif".format(rand_string)
+	msg.attach(part)
+	s = smtplib.SMTP('smtp.mailgun.org', 587)
 
-	print(r.status_code, r.text)
+	s.login(config['mailgun_sandbox'], config['mailgun_key'])
+	s.sendmail(msg['From'], msg['To'], msg.as_string())
+	s.quit()
+	# mail_id = request.form['email']
+	# rand_string = request.form['hash']
+	# request_url = 'https://api.mailgun.net/v3/{0}/messages'.format(config['mailgun_sandbox'])
+	# r = requests.post(
+	# 	request_url,
+	# 	auth=('api', config['mailgun_key']),
+	# 	data={
+	# 		'from': 'postmaster@srishti.sdslabs.co',
+	# 		'to': mail_id,
+	# 		'subject': 'Hello from SDSLabs',
+	# 		'text': 'Hello from Mailgun'
+	# 	},
+	# 	files=[("attachment", open("static/gifs/{}.gif".format(rand_string), 'rb'))]
+	# )
+
+	# print(r.status_code, r.text)
 
 	return "Success!"
 
