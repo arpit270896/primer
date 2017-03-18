df710e232bdce8dc716c130f61defa039585f803
Utkarsh Gupta <utkarsh357@gmail.com>
2017-03-18 06:51:43 +0530

    Add styles and handle gif

--- static/script.js
+++ static/script.js
@@ -4,31 +4,37 @@ var localMediaStream = null;
 var video = document.querySelector('video');
 
 function snapshot() {        
-		if (localMediaStream) {
-				ctx.drawImage(video, 0, 0);
-				var img = document.getElementById('CaptureImage');
-				// "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
-				img.src = canvas.toDataURL('image/jpg');
-
-				data = {
-					'image_data': img.src
-				}
-
-				$.ajax({
-					type: 'POST',
-					url: "/convert",
-					data: data,
-					success: function(resultData) {
-						console.log(resultData)
-					}
-				});
+	if (localMediaStream) {
+		ctx.drawImage(video, 0, 0);
+		console.log($("#MainContainer > img").length);
+		if($("#MainContainer > img").length >= 10) {
+			console.log($("#MainContainer").find('img:first-child'));
+			$("#MainContainer img:first-child").remove();
 		}
+		var img = document.createElement('img');
+		// "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
+		img.src = canvas.toDataURL('image/jpg');
+		$("#MainContainer").prepend(img);
+
+		data = {
+			'image_data': img.src
+		}
+
+		$.ajax({
+			type: 'POST',
+			url: "/convert",
+			data: data,
+			success: function(resultData) {
+				img.src = "/static/gifs/" + resultData + ".gif";
+			}
+		});
+	}
 }
 
 function hasGetUserMedia() {
-		// Note: Opera builds are unprefixed.
-		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-				navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	// Note: Opera builds are unprefixed.
+	return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia || navigator.msGetUserMedia);
 }
 
 function onFailSoHard(){
@@ -36,39 +42,40 @@ function onFailSoHard(){
 }
 
 function start() {
-		if (hasGetUserMedia()) {
-				if (navigator.webkitGetUserMedia)
-						navigator.getUserMedia = navigator.webkitGetUserMedia;
-				//var getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
+	if (hasGetUserMedia()) {
+		if (navigator.webkitGetUserMedia)
+			navigator.getUserMedia = navigator.webkitGetUserMedia;
+		//var getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
 
-				
-				//var gumOptions = { video: true, toString: function () { return 'video'; } };    
-				if (navigator.getUserMedia) {
-						navigator.getUserMedia({ video: true, audio: true }, function (stream) {
-								if (navigator.webkitGetUserMedia) {
-										video.src = window.webkitURL.createObjectURL(stream);
-								} else {
-										video.src = stream; // Opera
-								}
-								localMediaStream = stream;
-						}, onFailSoHard);
+		
+		//var gumOptions = { video: true, toString: function () { return 'video'; } };    
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({ video: true, audio: true }, function (stream) {
+				if (navigator.webkitGetUserMedia) {
+					video.src = window.webkitURL.createObjectURL(stream);
 				} else {
-						video.src = 'somevideo.webm'; // fallback.
+					video.src = stream; // Opera
 				}
+				localMediaStream = stream;
+			}, onFailSoHard);
+		} else {
+			video.src = 'somevideo.webm'; // fallback.
 		}
+	}
 }
 
 function stop() {
-		video = document.getElementById('sourcevid');
-		video.src = "";
+	video = document.getElementById('sourcevid');
+	video.src = "";
 }
 
 function ResizeCanvas() {
-		canvas.height = video.videoHeight;
-		canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	canvas.width = video.videoWidth;
 }
 
 $(document).ready(function () {
-		$('.button').button();
-		ctx = canvas.getContext('2d');
+	start();
+	$('.button').button();
+	ctx = canvas.getContext('2d');
 });
